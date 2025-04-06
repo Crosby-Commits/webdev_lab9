147c98259de9e3f909499a671db0d21e1cea458e
Crosby-Commits <jcrosby2@hawk.iit.edu>
2025-04-05 19:08:18 -0500

    add events

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function init(){
+  function myEventFunction(){
+    alert('Jamie Crosby: ');
+  }
+  
+  var d4 = document.getElementById('entrybutton');
+  d4.addEventListener('click', myEventFunction);
+  d4.addEventListener('click', function (){
+    console.log('I was Clicked!');
+    d4.style.backgroundColor = '#C00';
+    d4.innerHTML = '<strong>Goodbye</strong>';
+    
+  });
+}
+  window.addEventListener('load', init);
